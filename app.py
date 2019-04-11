0c156d67f6e7c1a2e2c44deab44358d93923cf2c
codejockey02 <codejockey02@gmail.com>
2019-04-11 11:18:05 +0530

    client file added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ Created on Sun Mar 17 03:03:04 2019
 import pickle
 from flask import Flask
 from flask import request
+from flask import render_template
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -19,24 +20,24 @@ def create_word_features(words):
     return my_dict
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 model = pickle.load(open("model.pkl", "rb"))
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello there, the flask application is now LIVE. "
+    return render_template('client.html')
 
 
 @app.route('/getrating', methods=['POST'])
 def predict():
     # getting the review from the user through the node API
-    review = request.get_json()['review']
+    review = request.form['review']
     words = word_tokenize(review)
     words = create_word_features(words)
     return (model.classify(words))
 
 
 if __name__ == "__main__":
-    app.run("127.0.0.1", "3004", debug=True)
+    app.run("127.0.0.1", "8080", debug=True)
